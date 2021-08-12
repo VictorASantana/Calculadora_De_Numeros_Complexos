22fb705b48921fff3a4738bc55926fb2b7718c0c
Victor de Almeida Santana <80405995+VictorASantana@users.noreply.github.com>
2021-08-11 21:49:56 -0300

    atualizacoes no work.js

--- funcionalidades/work.js
+++ funcionalidades/work.js
@@ -1,5 +1,6 @@
 /*Variaveis globais*/
 let numComplexos = 0;
+let vetorDeComplexos = [];
 
 /*Criação de novos complexos*/
 function criaComplexoCartesiano (real, imag){
@@ -20,6 +21,21 @@ function verificaNumComplexos(){
     else return false;
 }
 
+/*Funções de interação com o HTML*/ 
+let botaoCartesiano = document.getElementById("cartesiano");
+botaoCartesiano.onclick = function (){
+    let real = document.querySelector("#parteReal");
+    let imag = document.querySelector("#parteImag");
+    let valorReal = real.value;
+    let valorImag = imag.value;
+    valorReal = parseFloat(valorReal);
+    valorImag = parseFloat(valorImag);
+    console.log(valorImag+1);
+    console.log(valorReal+1);
+    let novoImag = criaComplexoCartesiano(valorReal, valorImag);
+    vetorDeComplexos.push(novoImag);
+    console.log(vetorDeComplexos);
+}
 
 
 
